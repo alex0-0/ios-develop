68fe6b16640fb911f8a70d98dad59f2ce5f4cae0
litp <litp@Ctrip.com>
2016-05-14 19:55:52 +0800

    Initial Commit

---
+++ OCRDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCRDemo
+//
+//  Created by ltp on 5/14/16.
+//  Copyright © 2016 ltp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
